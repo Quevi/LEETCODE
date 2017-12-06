133dde9412cfe287ee6dff6c6652b53997a7957c
QuQi <quqi@QuQideMacBook-Air.local>
2017-12-06 14:34:50 +0800

    bb

---
+++ q714 Transaction Fee/Common.h
@@ -0,0 +1,11 @@
+#ifndef __COMMON__
+#define __COMMON__
+	#include <iostream>
+	#include <memory>
+	#include <vector>
+	#include <map>
+	#include <utility>
+	#include <algorithm>
+
+	using namespace std;
+#endif
